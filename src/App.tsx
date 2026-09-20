697dfdc2e2aa21aeabb21cbe87ee6818cd93be2e
minhazulkader <minhazul.tonmoy26@gmail.com>
2026-09-20 12:28:20 +0600

    Update Header Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/header";
+
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      {/* Components will go here */}
+    <div className="min-h-screen bg-white">
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import logo from "../assets/logo-text.png";
+
+const Header = () => {
+    return (
+        <div className="sticky top-0 z-50 border-b border-gray-100 bg-white">
+            <nav className="flex h-16 w-[85%] max-w-7xl mx-auto items-center justify-between">
+                <img src={logo} alt="Dev Stack" className="w-24" />
+
+                <ul className="flex items-center gap-8 text-[13px] font-medium">
+                    <li><a href="#" className="text-[#D91B7E] transition"> Home </a></li>
+                    <li><a href="#" className="text-gray-500 transition hover:text-[#D91B7E]"> Technologies</a></li>
+                    <li><a href="#" className="text-gray-500 transition hover:text-[#D91B7E]"> Projects </a></li>
+                    <li><a href="#" className="text-gray-500 transition hover:text-[#D91B7E]" > About </a></li>
+                    <li><a href="#" className="text-gray-500 transition hover:text-[#D91B7E]" > Contact </a></li>
+                </ul>
+
+                <div className="flex items-center gap-6 text-[13px]">
+                    <button className="cursor-pointer rounded-full bg-gray-100 px-5 py-2 text-gray-600 transition-all duration-300 hover:bg-slate-600 hover:text-white"> Sign In </button>
+                    <button className="cursor-pointer rounded-full bg-[#D91B7E] px-5 py-2 text-white transition-all duration-300 hover:scale-105 hover:bg-[#A91461]"> Sign Up </button>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
