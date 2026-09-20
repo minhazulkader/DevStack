afd4c6874cda7d3f34d8669353d8a3d989cf0517
minhazulkader <minhazul.tonmoy26@gmail.com>
2026-09-20 17:59:50 +0600

    Add Technology components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/header";
 import Hero from "./components/hero";
+import Technologies from "./components/technology";
 
 const App = () => {
     return (
         <div>
             <Header />
             <Hero />
+            <Technologies />
         </div>
     );
 };
